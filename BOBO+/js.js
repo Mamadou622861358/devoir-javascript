dc6b30c5f567375273f37f344ad66f5d12cd32b1
Mamadou Bobo Baldé <mamadoubobobaldet@gmail.com>
2025-07-02 09:39:28 +0000

    devoir javascript

---
+++ BOBO+/js.js
@@ -0,0 +1,9 @@
+let fruits=["mangue","orange","ananas","fraise"];
+console.log("le premier fruit est :",fruits[0]);
+
+fruits.push("papaye");
+
+fruits.splice(1,1);
+console.log("la taille du tableau est:",fruits.length);
+
+console.log("le dernier fruit est :",fruits[fruits.length-1]);
